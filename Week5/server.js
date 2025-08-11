2ef5c79ef061dbc8dd7583cc8e7ce6a0ceed28e4
Ashjani <s224948386@deakin.edu.au>
2025-08-11 20:41:32 +1000

    Week5

--- Week5/server.js
+++ Week5/server.js
@@ -2,12 +2,16 @@ var express = require("express")
 var app = express()
 const projectsRoute = require('./routes/projects');
 
-// Mount the route at /api/projects
-app.use('/api/projects', projectsRoute);
-
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Mount the route at /api/projects
+app.use('/api/projects', projectsRoute);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 var port = process.env.port || 3000;
 app.listen(port, () => {
